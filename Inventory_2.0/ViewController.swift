3788cde31de0423c27504bb31c203ecafcec4f5f
DuranAdrian <96aduran@gmail.com>
2019-11-02 10:22:34 -0700

    Initial Commit

---
+++ Inventory_2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory_2.0
+//
+//  Created by Adrian Duran on 11/2/19.
+//  Copyright © 2019 Adrian Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
